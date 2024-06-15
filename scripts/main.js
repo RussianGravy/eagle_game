665bb0c041292a1291934ab32f0b97f0659119d4
RussianGravy <valentinejones1215@gmail.com>
2024-06-14 21:21:14 -0700

    resized window and played with game loop

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,12 @@ import {one} from './modules/one.mjs' //background
 import {two} from './modules/two.mjs' //player-space
 import {three} from './modules/three.mjs' //foreground
 
+var gameFrame = 0;
+const staggerFrame = 2;
+
+while(false){
+    gameFrame++;
+    if(gameFrame%staggerFrame == 0){
+        console.log(8);
+    }
+}
